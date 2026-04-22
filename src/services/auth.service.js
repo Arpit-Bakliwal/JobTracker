d5c19fe77fc635a2db8738c86c20d1ba0321b17f
Arpit Bakliwal <akb.7593@gmail.com>
2026-04-22 05:44:53 +0530

    Fix: added role in login response

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -105,6 +105,7 @@ const login = async ({ email, password }) => {
             id: user.id,
             name: user.name,
             email: user.email,
+            role: user.role,
             createdAt: user.createdAt,
         }, 
         accessToken,
