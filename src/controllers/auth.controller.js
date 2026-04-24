818445d500efad9dbb39633d8ef72e334f010ad2
Arpit Bakliwal <akb.7593@gmail.com>
2026-04-24 10:23:11 +0530

    Auth service samesite fix

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -54,7 +54,7 @@ const refresh = asyncHandler(async (req, res) => {
     res.cookie("refreshToken", result.refreshToken, {
         httpOnly: true,
         secure: process.env.NODE_ENV === "production",
-        sameSite: "Strict",
+        sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
         maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
